fe7a60ad5e823bc387c92af6fdcfbd4bf50c1f2a
Olesya Gorbacheva <554476744j@gmail.com>
2019-10-10 13:54:06 +0800

    анимация солнца

--- app/src/main/java/com/foxy/sunset/SunsetFragment.java
+++ app/src/main/java/com/foxy/sunset/SunsetFragment.java
@@ -1,15 +1,21 @@
 package com.foxy.sunset;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class SunsetFragment extends Fragment {
+
+    private View sceneView;
+    private View sunView;
+    private View skyView;
 
     public static SunsetFragment newInstance() {
         return new SunsetFragment();
@@ -19,6 +25,30 @@ public class SunsetFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sunset, container, false);
+
+        sceneView = view;
+        sunView = view.findViewById(R.id.sun);
+        skyView = view.findViewById(R.id.sky);
+
+        sceneView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startAnimation();
+            }
+        });
+
         return view;
+    }
+
+    private void startAnimation() {
+        float sunYStart = sunView.getTop();  // точка начала анимации
+        float sunYEnd = skyView.getHeight(); // конец анимации
+
+        ObjectAnimator heightAnimator = ObjectAnimator
+                .ofFloat(sunView, "y", sunYStart, sunYEnd)
+                .setDuration(3000);
+        heightAnimator.setInterpolator(new AccelerateInterpolator()); // ускорение солнца
+
+        heightAnimator.start();
     }
 }
